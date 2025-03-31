13f5711f6258a234b61924259f23841db146514a
thk <836474867@qq.com>
2025-03-31 09:15:49 +0100

    First commit

---
+++ training-appointment-start/src/main/java/com/myfitnessapp/trainingappointment/TrainingAppointmentApplication.java
@@ -0,0 +1,13 @@
+package com.myfitnessapp.trainingappointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingAppointmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingAppointmentApplication.class, args);
+    }
+
+}
